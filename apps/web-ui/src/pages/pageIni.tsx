12e9d0f29273d1bd65f258b408edba78f5034e46
Juan Baquero <juan.baquero.z@outlook.com>
2024-07-29 17:26:55 -0500

    ui-Fix to deploy

--- apps/web-ui/src/pages/pageIni.tsx
+++ apps/web-ui/src/pages/pageIni.tsx
@@ -1,15 +1,4 @@
-import { useFetchUsers } from '../services/inicial';
 import ParentComponent from './parent';
-
-function FetchUsers() {
-  const { isLoading, error, data } = useFetchUsers();
-
-  if (isLoading) return 'Loading...';
-
-  if (error) return 'An error has occurred: ' + (error as Error).message;
-
-  return <div>{data}</div>;
-}
 
 function PageIni() {
   return (
