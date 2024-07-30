1fc12f4c3efc75e5df8d09fb758d21b49d4441df
Juan Baquero <juan.baquero.z@outlook.com>
2024-07-30 16:27:29 -0500

    ui-fix deploy

--- apps/web-ui/vite.config.ts
+++ apps/web-ui/vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    "process.platform": JSON.stringify(process.platform),
+    "process.env": {},
+  },
   plugins: [react()],
   base: "/radio-alert",
 });
